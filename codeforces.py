ca4d0572c1bd18347ed40f64c4dd0118ce9e85b1
To Duc <20toduc01@gmail.com>
2020-05-05 00:29:35 +0700

    new class: CodeforcesUser

--- codeforces.py
+++ codeforces.py
@@ -1,11 +1,21 @@
 import requests, json
-async def test(context):
-    url = "https://codeforces.com/api/blogEntry.comments?blogEntryId=76830"
-    rawData = requests.get(url)
-    jsonData = rawData.json()
-    if jsonData['status'] == 'OK':
-        data = jsonData['result']
-        response = ""
-        for comment in data: 
-            response += comment['commentatorHandle'] + ", upvote: " + str(comment['rating']) + '\n'
-    await context.send(response)
+
+class CodeforcesUser:
+    def __init__(self, query):
+        url = "https://codeforces.com/api/user.info?handles=" + query
+        rawData = requests.get(url)
+        jsonData = rawData.json()
+        if jsonData['status'] == 'OK':
+            data = jsonData['result']
+            data = data[0]
+            self.isNULL = False
+            self.handle = data['handle']
+            self.rating = data['rating']
+            self.rank = data['rank']
+            if 'country' in data: self.country = data['country']
+
+    handle = ""
+    isNULL = True
+    rating = -1
+    rank = "Undefined"
+    country = "Undefined"
