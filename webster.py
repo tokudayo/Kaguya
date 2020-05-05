c2c7bc0db1caa79d3dc29de634cbdd4ee83e7511
To Duc <20toduc01@gmail.com>
2020-05-05 21:15:19 +0700

    structural changes in class Word

--- webster.py
+++ webster.py
@@ -2,19 +2,14 @@ import requests, json
 url = 'https://dictionaryapi.com/api/v3/references/learners/json/'
 APIKey = '?key=281250e6-3e7e-4487-9b65-768400628bc0'
 
-class Word:
-    def __init__(self, word, definition, stems):
-        self.isNULL = False
-        self.word = word
-        self.definition = definition
-        self.stems = stems
-
+class Word(object):
     word = ""
     definition = []
     stems = []
     isNULL = True
 
-    def getWord(self, query):
+    def __init__(self, query):
+        self.isNULL = False
         rawData = requests.get(url + query + APIKey)
         jsonData = rawData.json()
         data = jsonData[0]
@@ -22,5 +17,7 @@ class Word:
             rawData = requests.get(url + data + APIKey)
             jsonData = rawData.json()
             data = jsonData[0]
-            
-        self = Word(data['meta']['id'], data['shortdef'], data['meta']['stems'])
+        self.word = data['meta']['id']
+        self.definition = data['shortdef']
+        self.stems = data['meta']['stems']
+        return
