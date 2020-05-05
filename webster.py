4a6e62646f4b302fbe3a9dda475ad0ac11145182
To Duc <20toduc01@gmail.com>
2020-05-05 19:02:14 +0700

    Creation of class Word

--- webster.py
+++ webster.py
@@ -2,18 +2,25 @@ import requests, json
 url = 'https://dictionaryapi.com/api/v3/references/learners/json/'
 APIKey = '?key=281250e6-3e7e-4487-9b65-768400628bc0'
 
-def defineWord(query):
-    rawData = requests.get(url + query + APIKey)
-    jsonData = rawData.json()
-    data = jsonData[0]
-    if type(data) != dict: #data is a string
-        print("Requested word not found in database, showing result for " + data)
-        rawData = requests.get(url + data + APIKey)
+class Word:
+    def __init__(self, word, definition, stems):
+        self.isNULL = False
+        self.word = word
+        self.definition = definition
+        self.stems = stems
+
+    word = ""
+    definition = []
+    stems = []
+    isNULL = True
+
+    def getWord(self, query):
+        rawData = requests.get(url + query + APIKey)
         jsonData = rawData.json()
         data = jsonData[0]
-        print("Showing definition for " + data["meta"]["id"])
-    for auto in data['shortdef']: print(auto)
-    print("Stems")
-    for auto in data['meta']['stems']: print(auto,end=', ')
-
-defineWord("concac")
+        if type(data) != dict: #data is a string
+            rawData = requests.get(url + data + APIKey)
+            jsonData = rawData.json()
+            data = jsonData[0]
+            
+        self = Word(data['meta']['id'], data['shortdef'], data['meta']['stems'])
