72c2d139157a1b5e23e09f06b668119edef074bc
To Duc <20toduc01@gmail.com>
2020-05-07 00:39:47 +0700

    More fixing and completion

--- wiki.py
+++ wiki.py
@@ -1,14 +1,30 @@
-from wikipedia import search, summary, WikipediaPage
+import wikipedia
 
 class WikiPage():
     def __init__(self, args):
         self.title = ""
+        self.shortSummary = ""
+        self.url = "https://en.wikipedia.org/wiki/"
         query = ""
         print(args)
         for auto in args:
             query += auto + " "
-        suggestions = search(query)
+        suggestions = wikipedia.search(query)
         print(suggestions)
         if len(suggestions):
             self.title = suggestions[0]
-        self.shortSummary = summary(query, sentences=2)
+            urlSuffix = ""
+            for auto in self.title.split()[:len(self.title.split())-1]: urlSuffix += auto + "_"
+            urlSuffix += self.title.split()[len(self.title.split())-1]
+            self.url += urlSuffix
+            print(self.url)
+            try:
+                summaryQuery = "" # There is a odd bug in the wikipedia package. wikipedia.summary("mercury (planet)")
+                for char in self.title:
+                    if char != ')' and char != '(':
+                        summaryQuery += char
+                self.shortSummary = wikipedia.summary(summaryQuery, sentences=2)
+            except wikipedia.exceptions.DisambiguationError:
+                self.title = "Topic too broad."
+        else:
+            self.title = "No article found."
