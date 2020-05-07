fa9f0fae1d348c6fe15defff0ac613be33fec3ea
To Duc <20toduc01@gmail.com>
2020-05-08 01:11:28 +0700

    Rating graph function (on dev)

--- codeforces.py
+++ codeforces.py
@@ -1,7 +1,17 @@
 import requests, json
 
 class CodeforcesUser:
+
+
     def __init__(self, query):
+        self.handle = ""
+        self.isNULL = True
+        self.rating = 0
+        self.rank = "No rank"
+        self.country = "Unknown"
+        self.avatar = ""
+        self.ratingChange = []
+        
         url = "https://codeforces.com/api/user.info?handles=" + query
         rawData = requests.get(url)
         jsonData = rawData.json()
@@ -14,10 +24,10 @@ class CodeforcesUser:
             if 'rank' in data: self.rank = data['rank']
             self.avatar = data['avatar']
             if 'country' in data: self.country = data['country']
+            
+            rawData = requests.get("https://codeforces.com/api/user.rating?handle=" + self.handle)
+            ratingData = rawData.json()['result']
+            for auto in ratingData:
+                self.ratingChange.append((auto['ratingUpdateTimeSeconds'],auto['newRating']))
+            jsonData = rawData.json()
 
-    handle = ""
-    isNULL = True
-    rating = 0
-    rank = "No rank"
-    country = "Unknown"
-    avatar = ""
