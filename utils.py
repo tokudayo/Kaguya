2d0241e47946e7f7ff4f81a7ce6f829660f0ee6f
To Duc <20toduc01@gmail.com>
2020-05-13 01:33:15 +0700

    Added new utility function: yesnoReact.

--- utils.py
+++ utils.py
@@ -1,4 +1,20 @@
+import discord, asyncio
+
 def dumpToFile(**kwargs):
     f = open(kwargs['path'],'w+', encoding='utf-8')
     f.write(kwargs['response'])
     f.close()
+
+async def yesnoReact(context, message):
+    msg = await context.send(message)
+    await msg.add_reaction('👍')
+    await msg.add_reaction('👎')
+    try:
+        reaction, user = await context.bot.wait_for('reaction_add', timeout=15.0, check=lambda reaction, user: user == context.author and (str(reaction.emoji) == '👍' or str(reaction.emoji) == '👎'))
+    except asyncio.TimeoutError:
+        return "TIMEOUT"
+    else:
+        return {
+            'reaction': reaction,
+            'user': user,
+        }
